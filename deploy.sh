7bd0165e559d5e3b95d95c335e9875c2683dc4d0
Emre Keskin <emrekeskinmac@users.noreply.github.com>
2017-08-23 19:53:53 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+version=$(echo git rev-parse HEAD) 
+
+npm install
+npm test
+echo "Test Güzel Geçti"
+docker build -t emrekeskinmac/node-test:$version .
+docker push emrekeskinmac/node-test:$version
+ssh root@46.101.248.17 "docker service update --image emrekeskinmac/node-test:$version test" 
+echo "Oldu"
